4a898a6a63ca16e8de05f43371a0204e4e637017
Kat <kemetcalf3.14@gmail.com>
2022-03-16 15:08:21 -0700

    React 101 work

--- react101/src/App.js
+++ react101/src/App.js
@@ -2,16 +2,74 @@ import './App.css';
 // import Item from './MyItem';
 import React from 'react';
 
-class StarWars extends React.Component{
+class AffiliationRow extends React.Component {
   render() {
     return (
+      <li>
+       {this.props.organization}
+      </li>
+    )
+  }
+}
+
+class StarWars extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      loadedCharacter: false,
+      image: null,
+      name: null,
+      height: null,
+      homeworld: null,
+      affiliations: [],
+    };
+  }
+  getNewCharacter() {
+    const randomNumber = Math.round(Math.random() * 88);
+    const url = `https://akabab.github.io/starwars-api/api/id/${randomNumber}.json`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          image: data.image,
+          name: data.name,
+          height: data.height,
+          homeworld: data.homeworld,
+          affiliations: data.affiliations,
+          loadedCharacter: true,
+        })
+      })
+
+  };
+
+  render() {
+    
+      const groups = this.state.affiliations.map((group, i) => {
+        return <AffiliationRow key={i} organization={group}/>
+      })
+    
+    return (
       <div>
-        <h1>Name</h1>
-        <p>Height (cm)</p>
-        <p>Homeworld:</p>
-        <ul>
-          <li>Films in here</li>
-        </ul>
+        {
+          this.state.loadedCharacter && 
+          <div>
+            <img src={this.state.image} alt="character headshot"></img>
+            <h1>{this.state.name}</h1>
+            <p>{this.state.height} m</p>
+            <p>Homeworld: {this.state.homeworld}</p>
+            <ul>
+              {groups}
+            </ul>
+          </div>
+        }
+
+        <button 
+          type="button" 
+          onClick={() => this.getNewCharacter()}
+          className="btn"
+          >
+          Randomize Character
+        </button>
       </div>
     )
   }
