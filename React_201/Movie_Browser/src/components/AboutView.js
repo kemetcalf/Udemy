25b3221542ba3540499f588ae19a1d0fcb15c8d0
Kat <kemetcalf3.14@gmail.com>
2023-02-27 17:35:06 -0800

    Lorem clean up

--- React_201/Movie_Browser/src/components/AboutView.js
+++ React_201/Movie_Browser/src/components/AboutView.js
@@ -5,12 +5,9 @@ const AboutView = () => {
 		<>
 			<Hero text="About Us" />
 			<p className="text-center p-5">
-				Cat ipsum dolor sit amet, kitty pounce, trip, faceplant you didn't see
-				that no you didn't definitely didn't lick, lick, lick, and preen away
-				the embarrassment hate dog. My water bowl is clean and freshly
-				replenished, so i'll drink from the toilet climb leg i is not fat, i is
-				fluffy but love and coo around boyfriend who purrs and makes the perfect
-				moonlight eyes so i can purr and swat the glittery gleaming yarn
+				This here movie browser is powered by React and styled with Bootstrap,
+				with data provided by The Movie Database's API at&nbsp;
+				<a href="www.themoviedb.org">www.themoviedb.org</a>
 			</p>
 		</>
 	);
--- React_201/Movie_Browser/src/components/Home.js
+++ React_201/Movie_Browser/src/components/Home.js
@@ -3,18 +3,12 @@ import Hero from "./Hero";
 const Home = () => {
 	return (
 		<>
-			<Hero text="Welcome to React 201" />
+			<Hero text="Let's watch a movie!" />
 			<div className="container">
 				<div className="row">
 					<div className="col-lg-8 offset-lg-2 my-5">
 						<p className="lead">
-							Cat ipsum dolor sit amet, kitty pounce, trip, faceplant you didn't
-							see that no you didn't definitely didn't lick, lick, lick, and
-							preen away the embarrassment hate dog. My water bowl is clean and
-							freshly replenished, so i'll drink from the toilet climb leg i is
-							not fat, i is fluffy but love and coo around boyfriend who purrs
-							and makes the perfect moonlight eyes so i can purr and swat the
-							glittery gleaming yarn
+							Start typing a title into the search bar above to get started...
 						</p>
 					</div>
 				</div>
